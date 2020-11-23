cc4b29872a49bc29746f4cc917dfffa4f5dfaffa
giobanny23 <edgar_2_62@hotmail.com>
2020-11-22 20:24:43 -0600

    FormWithCustomHook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { FormWithCustomHook } from './componets/02-useEffect/FormWithCustomHook';
 // import {HookApp} from './HookApp';
 //import { CounterApp } from './componets/01-useState/CounterApp';
 //import { CounterWithCustomHook } from './componets/01-useState/CounterWithCustomHook';
-import { SimpleForm } from './componets/02-useEffect/SimpleForm';
+//import { SimpleForm } from './componets/02-useEffect/SimpleForm';
 
 ReactDOM.render(
   
-    <SimpleForm  />,
+    <FormWithCustomHook  />,
  
   document.getElementById('root')
 );
