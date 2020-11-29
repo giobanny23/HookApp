cb7dfc1537c1470972c9ab5f84aaf7554c901846
giobanny23 <edgar_2_62@hotmail.com>
2020-11-28 22:24:15 -0600

    Nueva actualizacion de datos

--- src/componets/04-useRef/FocusScreen.js
+++ src/componets/04-useRef/FocusScreen.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 import '../02-useEffect/effects.css';
 
 export const FocusScreen = () => {
+
+    const ref = useRef();
+    console.log(ref);  
+
 
     const handleClick =() =>{
         document.querySelector('input').select();
