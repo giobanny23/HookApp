11c97cb5685d228b3624f86fd4aea3aa677c3b9f
giobanny23 <edgar_2_62@hotmail.com>
2020-12-25 13:26:22 -0600

    Nuevos cambios

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+// import { HookApp } from './HookApp';
+// import { CounterApp } from './components/01-useState/CounterApp';
+// import { CounterWithCustomHook } from './components/01-useState/CounterWithCustomHook';
+// import { SimpleForm } from './components/02-useEffect/SimpleForm';
+// import { FormWithCustomHook } from './components/02-useEffect/FormWithCustomHook';
+// import { MultipleCustomHooks } from './components/03-examples/MultipleCustomHooks';
+// import { RealExampleRef } from './components/04-useRef/RealExampleRef';
+// import { Layout } from './components/05-useLayoutEffect/Layout';
+// import { Memorize } from './components/06-memos/Memorize';
+// import { MemoHook } from './components/06-memos/MemoHook';
+// import { CallbackHook } from './components/06-memos/CallbackHook';
+// import { Padre } from './components/07-tarea-memo/Padre';
+// import { TodoApp } from './components/08-useReducer/TodoApp';
+import { MainApp } from './components/09-useContext/MainApp';
 
-//import { FocusScreen } from './componets/04-useRef/FocusScreen';
-//import { Layout } from './componets/05-useLayoutEffect/Layout';
-//import { CallbackHook } from './componets/06-memos/CallbackHook';
-//import { Padre } from './componets/07-tarea-memo/Padre';
-import { TodoApp } from './componets/08-useReducer/TodoApp';
-//import { ShowIncrement } from './componets/06-memos/ShowIncrement';
-//import { FormWithCustomHook } from './componets/02-useEffect/FormWithCustomHook';
-// import {HookApp} from './HookApp';
-//import { CounterApp } from './componets/01-useState/CounterApp';
-//import { CounterWithCustomHook } from './componets/01-useState/CounterWithCustomHook';
-//import { SimpleForm } from './componets/02-useEffect/SimpleForm';
+
 
 ReactDOM.render(
-  
-    <TodoApp />,
- 
+    <MainApp />,
   document.getElementById('root')
 );
-
-
